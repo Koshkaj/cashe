139625b528b148e36dd7a78edb6e6a1514d63b29
koshkaj <dkoshkeli@gmail.com>
2023-06-11 15:25:08 +0400

    removed unnecessary code

--- main.go
+++ main.go
@@ -1,16 +1,12 @@
 package main
 
 import (
-	"context"
 	"flag"
-	"fmt"
 	"log"
 	"os"
 	"os/signal"
-	"time"
 
 	"github.com/Koshkaj/cashe/cache"
-	"github.com/Koshkaj/cashe/client"
 )
 
 func main() {
@@ -36,28 +32,4 @@ func main() {
 	terminate := make(chan os.Signal, 1)
 	signal.Notify(terminate, os.Interrupt, os.Kill)
 	<-terminate
-}
-
-func SendCommand() {
-	for i := 0; i < 100; i++ {
-		go func(i int) {
-			c, err := client.New(":3000", client.Options{})
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			time.Sleep(time.Second * 2)
-			var (
-				key   = []byte(fmt.Sprintf("key_%d", i))
-				value = []byte(fmt.Sprintf("value_%d", i))
-			)
-			err = c.Set(context.Background(), key, value, 20)
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			_, err = c.Get(context.Background(), key)
-			c.Close()
-		}(i)
-	}
 }
